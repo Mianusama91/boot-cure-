8515f5d3673630b383e4df2909f4de28a722f97a
Usama Rehman <108446521+Mianusama91@users.noreply.github.com>
2022-07-04 12:32:50 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ElectricBill
+{
+    abstract class ElectricBill
+    {
+        protected double totalUnits;
+        protected double bill;
+        protected double taxPercent;
+        protected double taxAmount;
+        public ElectricBill()
+        {
+            this.taxAmount = 1;
+        }
+        public abstract void CalculateBill();
+        public abstract void CalculateTax();
+        public void SetUnits(double units)
+        {
+            this.totalUnits = units;
+        }
+        public void SetTax(double tax)
+        {
+            this.taxPercent = tax;
+        }
+        public void DisplayBill()
+        {
+            Console.WriteLine($"Your bill without tax = {this.bill}\nYour tax = {this.taxAmount}\nTotal Payable Bill = {this.taxAmount + this.bill}");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please Enter number From Menu ");
+            Console.WriteLine("1 For Rasidential \nany Other For Commercial");
+            int choice = Convert.ToInt32(Console.ReadLine());
+            ElectricBill obj;
+            if (choice == 1)
+            {
+                obj = new Residential();
+                obj.SetTax(13);
+            }
+            else
+            {
+                obj = new Commercial();
+                obj.SetTax(17);
+            }
+            Console.WriteLine("Please Enter How Many Units were consumed");
+            double units = Convert.ToDouble(Console.ReadLine());
+            obj.SetUnits(units);
+            obj.CalculateBill();
+            obj.CalculateTax();
+            obj.DisplayBill();
+
+        }
+    }
+    class Residential : ElectricBill
+    {
+        public override void CalculateBill()
+        {
+            if (this.totalUnits > 500)
+            {
+                this.bill += (totalUnits - 500) * 69;
+                this.totalUnits = 500;
+            }
+            if (this.totalUnits > 200)
+            {
+                this.bill += (totalUnits - 200) * 23;
+                this.totalUnits = 200;
+            }
+            if (this.totalUnits > 100)
+            {
+                this.bill += (totalUnits - 100) * 17;
+                this.totalUnits = 100;
+            }
+            if (this.totalUnits > 0)
+            {
+                this.bill += totalUnits * 5;
+            }
+
+        }
+
+        public override void CalculateTax()
+        {
+            this.taxAmount = (this.bill * this.taxPercent) / 100;
+        }
+    }
+
+    class Commercial : ElectricBill
+    {
+        public override void CalculateBill()
+        {
+            if (this.totalUnits > 500)
+            {
+                this.bill += (totalUnits - 500) * 79;
+                this.totalUnits = 500;
+            }
+            if (this.totalUnits > 200)
+            {
+                this.bill += (totalUnits - 200) * 23;
+                this.totalUnits = 200;
+            }
+            if (this.totalUnits > 100)
+            {
+                this.bill += (totalUnits - 100) * 21;
+                this.totalUnits = 100;
+            }
+            if (this.totalUnits > 0)
+            {
+                this.bill += totalUnits * 8;
+            }
+
+        }
+       
+        public override void CalculateTax()
+        {
+            this.taxAmount = (this.bill * this.taxPercent) / 100;
+        }
+    }
+}
---
+++ salaryproject.cs
@@ -0,0 +1,104 @@
+﻿using System;
+
+namespace salaryproject
+{
+    abstract class Emplooye
+    {
+        protected double baseSalary;
+        protected double fuelAllowance;
+        protected double medicalAllowance;
+        protected double grossPay;
+        abstract public void SetMedicalAllowance();
+        abstract public void SetFuelAllowance();
+
+        public void SetBaseSalary()
+        {
+            this.baseSalary = 1500;
+        }
+        public double GetBaseSalary()
+        {
+            return this.baseSalary;
+        }
+
+        public double GetFuelAllowance()
+        {
+            return this.fuelAllowance;
+        }
+
+        public double GetMedicalAllowance()
+        {
+            return this.medicalAllowance;
+        }
+        public double GrossPay()
+        {
+            this.grossPay = this.GetBaseSalary() + this.GetFuelAllowance() + this.GetMedicalAllowance();
+            return this.grossPay;
+        }
+    }
+    class Manager : Emplooye
+    {
+        public override void SetFuelAllowance()
+        {
+            this.fuelAllowance = 250;
+        }
+
+        public override void SetMedicalAllowance()
+        {
+            this.medicalAllowance = 1000;
+        }
+    }
+    class Analysts : Emplooye
+    {
+        public override void SetFuelAllowance()
+        {
+            this.fuelAllowance = 150;
+        }
+
+        public override void SetMedicalAllowance()
+        {
+            this.medicalAllowance = 800;
+        }
+    }
+    class Engineer : Emplooye
+    {
+        public override void SetFuelAllowance()
+        {
+            this.fuelAllowance = 100;
+        }
+
+        public override void SetMedicalAllowance()
+        {
+            this.medicalAllowance = 500;
+        }
+    }
+    class SalarySlip
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter the designation ");
+            Console.WriteLine("1  Manager \n2  Engineer \n3  Analysts");
+            int input = Convert.ToInt32(Console.ReadLine());
+          
+            Emplooye obj;
+            if ( input == 1)
+            {
+                obj = new Manager();
+               
+            }
+            else if (input == 2)
+            {
+                obj = new Engineer();
+            }
+            else
+            {
+                obj = new Analysts();
+            }
+            obj.SetBaseSalary();
+            obj.SetFuelAllowance();
+            obj.SetMedicalAllowance();
+            Console.WriteLine(  "Employee Gross salaray is  =  " + obj.GrossPay()  + "$");
+
+        }
+    }
+ 
+}
